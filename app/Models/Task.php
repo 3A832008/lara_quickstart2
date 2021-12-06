8e62f73426c814f580fc99152197c7e1be4d7ac1
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2021-12-06 23:45:49 +0800

    Eloquent關係

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
+$user = App\User::find(1);
+
+foreach ($user->tasks as $task) {
+    echo $task->name;
 }
