d87cd686b6688f12cdcfdd26e46d511bf355f645
{hui lin chu} <3a832008@gm.student.ncut.edu.tw>
2021-12-13 22:06:26 +0800

    將回傳視圖的程式碼,修改路由

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,6 +23,6 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
 
-Route::get('/tasks', 'TaskController@index');
-Route::post('/task', 'TaskController@store');
-Route::delete('/task/{task}', 'TaskController@destroy');
+Route::get('/tasks', [\App\Http\Controllers\TaskController::class, 'index'])->name('tasks.index');
+Route::post('/task', [\App\Http\Controllers\TaskController::class, 'store'])->name('tasks.store');
+Route::delete('/task/{task}', [\App\Http\Controllers\TaskController::class, 'destroy'])->name('tasks.destroy');
